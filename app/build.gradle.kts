ac5bee618d838c812142a3111f810fb0a4299e3b
Florencia Morelli <florenciamorelliIT@gmail.com>
2024-08-23 20:24:54 -0300

    DogViewHolder and DogAdapter fully implemented + Picasso usage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,5 +49,5 @@ dependencies {
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.picasso:2.5.2")
+    implementation("com.squareup.picasso:picasso:2.5.2")
 }
